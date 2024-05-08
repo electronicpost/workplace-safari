08e5a5dd819bc3bb5e7637c697e7e2eceadd871b
Mick Collins <mick.collins@advania.co.uk>
2024-05-08 12:20:55 +0100

    wps-walk

--- tvmaze/wps-show-submit-walk.py
+++ tvmaze/wps-show-submit-walk.py
@@ -1,28 +1,13 @@
 import requests
 
+# First search a TV Show using Thunder Client
+# SEARCH_URL = https://api.tvmaze.com/search/shows
+
 url='https://wps-tvmaze.azurewebsites.net/api/submit'
 
-# Build query string for parameters 'name' and 'show_id'
-
-id = 100
-
-#for id in range(100, 20000):
-query_string = {
-        'name': 'Mick',
-        'show_id': str(id)
-    }
-
-    # Make request using requests.get() 
-
-response = requests.get(url, params=query_string)
-    
-print(response, response.text)
-
-    # Print response code and response text
+# Build query_string dictionary for 'name' and show_id
 
 
+# response variable to requests.get(url, params=qs)
 
-# Print response code and response text
-
-
-
+# Print response
